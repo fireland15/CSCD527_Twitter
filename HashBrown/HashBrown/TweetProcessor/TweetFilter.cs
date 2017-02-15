d797fa42a68d156350649ebc8cba8cae5035e5c7
UNIT-BETH\Beth <andersonbt92@hotmail.com>
2017-02-14 20:05:28 -0800

    Beginning of filter and created constructor for tweeter object

--- HashBrown/HashBrown/TweetProcessor/TweetFilter.cs
+++ HashBrown/HashBrown/TweetProcessor/TweetFilter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Shared;
 using Tweetinvi.Models;
+using Tweetinvi.Models.Entities;
+using System.Collections.Generic;
 
 namespace TweetProcessor
 {
@@ -8,7 +10,22 @@ namespace TweetProcessor
     {
         public Tweeter FilterTweet(ITweet tweet)
         {
-            throw new NotImplementedException();
+            ITweetEntities media = tweet.Entities;
+            if(media != null)
+            { return null; }
+
+            List<IHashtagEntity> hashTags = tweet.Hashtags;
+            if (hashTags == null)
+            { return null; }
+
+            List<IUrlEntity> urls = tweet.Urls;
+            if (urls != null)
+            { return null; }
+
+            //need to figureout what the difference is between text and full text
+            Tweeter ourTweeter = new Tweeter(tweet.FullText, tweet.Hashtags);
+
+            return ourTweeter;
         }
     }
 }
