f4223f3adb334c84399ad81ccbf0ad6b3a55b29c
fireland15 <fireland15@my.whitworth.edu>
2017-02-16 17:04:10 -0800

    Added project so we can remotely execute sql on pipelinedb@www.forrestireland.com.

---
+++ HashBrown/HashBrown/PipelineConsole/PipelineCommander.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace PipelineConsole
+{
+    public class PipelineCommander
+    {
+        private readonly NpgsqlConnection _connection;
+
+        public PipelineCommander(NpgsqlConnection connection)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            _connection = connection;
+
+            if (_connection.FullState != System.Data.ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.Write(">");
+                string command = Console.ReadLine();
+
+                if (command.Equals("quit", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    break;
+                }
+
+                NpgsqlCommand sqlCommand = new NpgsqlCommand(command, _connection);
+
+                try
+                {
+                    NpgsqlDataReader rdr = sqlCommand.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine(rdr.ToString());
+                    }
+                }
+                catch (NpgsqlException ex)
+                {
+                    Console.WriteLine($"Exception Encountered: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("Quitting");
+        }
+    }
+}
---
+++ HashBrown/HashBrown/PipelineConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+using System;
+using System.Configuration;
+
+namespace PipelineConsole
+{
+    class Program
+    {
+        static void PrintOpeningMessage(string conStr)
+        {
+            Console.WriteLine($"Connecting using the connection string: \n{conStr}");
+            Console.WriteLine();
+            Console.WriteLine("Enter sql commands for pipelinedb");
+            Console.WriteLine();
+            Console.WriteLine("type 'quit' when you are finished to close the connection");
+        }
+
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.AppSettings["connectionString"].ToString();
+
+            PrintOpeningMessage(connectionString);
+
+            using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+            {
+                con.Open();
+                
+                PipelineCommander commander = new PipelineCommander(con);
+
+                commander.Run();
+            }
+
+
+        }
+    }
+}
