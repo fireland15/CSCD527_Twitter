a492464c96eb7d286634ac607275309244fadfd1
fireland15 <fireland15@my.whitworth.edu>
2017-02-16 20:11:54 -0800

    Implemented PipelineRepository

--- HashBrown/HashBrown/Shared/PipelineRepository.cs
+++ HashBrown/HashBrown/Shared/PipelineRepository.cs
@@ -1,20 +1,91 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace Shared
 {
     public class PipelineRepository : IPipelineRepository
     {
-        public PipelineRepository()
+        private readonly NpgsqlConnection _connection;
+
+        public PipelineRepository(NpgsqlConnection connection)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            _connection = connection;
+
+            if (_connection.State != ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
 
         public void Insert(WordHashtagPair pair)
         {
-            throw new NotImplementedException();
+            string sql = "INSERT INTO twitter_stream (word, hashtag) VALUES (@word, @hashtag);";
+                        
+            using (NpgsqlCommand command = new NpgsqlCommand(sql, _connection))
+            {
+                var transaction = _connection.BeginTransaction();
+                command.Transaction = transaction;
+
+                command.Parameters.Add("@word", NpgsqlTypes.NpgsqlDbType.Varchar).Value = pair.Word;
+                command.Parameters.Add("@hashtag", NpgsqlTypes.NpgsqlDbType.Varchar).Value = pair.HashTag;
+                command.ExecuteNonQuery();
+
+                try
+                {
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                }
+            }
         }
 
         public void InsertMany(IEnumerable<WordHashtagPair> pairs)
         {
-            throw new NotImplementedException();
+            string sql = "INSERT INTO twitter_stream (word, hashtag) VALUES (@word, @hashtag);";
+
+            using (NpgsqlTransaction transaction = _connection.BeginTransaction())
+            {
+                using (NpgsqlCommand command = _connection.CreateCommand())
+                {
+                    command.Transaction = transaction;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = sql;
+                    command.Parameters.Add(new NpgsqlParameter("@word", NpgsqlTypes.NpgsqlDbType.Varchar));
+                    command.Parameters.Add(new NpgsqlParameter("@hashtag", NpgsqlTypes.NpgsqlDbType.Varchar));
+
+                    try
+                    {
+                        foreach (var pair in pairs)
+                        {
+                            command.Parameters[0].Value = pair.Word;
+                            command.Parameters[1].Value = pair.HashTag;
+                            if (command.ExecuteNonQuery() != 1)
+                            {
+                                throw new InvalidProgramException();
+                            }
+                        }
+
+                        transaction.Commit();
+                    }
+                    catch (InvalidProgramException)
+                    {
+                        /* Handle as we see fit */
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                    }
+                }
+            }
         }
     }
 }
---
+++ HashBrown/HashBrown/Test.Shared/TestRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Npgsql;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Shared
+{
+    [TestClass]
+    public class TestRepository
+    {
+        [TestMethod]
+        public void TestSingleInsert()
+        {
+            string conStr = "Server=www.forrestireland.com;Port=5432;User Id=CharlieBrown;Password=AcademicCoffeeBean59943;Database=pipeline;";
+
+            using (NpgsqlConnection con = new NpgsqlConnection(conStr))
+            {
+                IPipelineRepository repo = new PipelineRepository(con);
+                WordHashtagPair pair = new WordHashtagPair
+                {
+                    Word = "banana",
+                    HashTag = "yellow"
+                };
+
+                repo.Insert(pair);
+            }
+
+            // Manual verification that insert appeared in database;
+            // Made by Forrest
+        }
+
+        [TestMethod]
+        public void TestInsertMany()
+        {
+            string conStr = "Server=www.forrestireland.com;Port=5432;User Id=CharlieBrown;Password=AcademicCoffeeBean59943;Database=pipeline;";
+
+            using (NpgsqlConnection con = new NpgsqlConnection(conStr))
+            {
+                IPipelineRepository repo = new PipelineRepository(con);
+                IEnumerable<WordHashtagPair> pairs = new List<WordHashtagPair>
+                {
+                    new WordHashtagPair
+                    {
+                        Word = "banana",
+                        HashTag = "yellow"
+                    },
+
+                    new WordHashtagPair
+                    {
+                        Word = "banana",
+                        HashTag = "curvy"
+                    },
+
+                    new WordHashtagPair
+                    {
+                        Word = "banana",
+                        HashTag = "yellow"
+                    }
+                };
+
+                repo.InsertMany(pairs);
+            }
+        }
+    }
+}
