24a0ecaca07fdbcde98b047adb48447ba2f8ceb8
fireland15 <fireland15@my.whitworth.edu>
2017-03-02 19:29:49 -0800

    updates

--- HashBrown/HashBrown/HashtagGenerator/AprioriRepository.cs
+++ HashBrown/HashBrown/HashtagGenerator/AprioriRepository.cs
@@ -1,4 +1,5 @@
-﻿using Shared.Interfaces;
+﻿using Npgsql;
+using Shared.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,45 @@ namespace HashtagGenerator
 {
     public class AprioriRepository : IAprioriRepository
     {
-        public AprioriRepository(NpgsqlConnection con)
-        {
+        private readonly NpgsqlConnection _connection;
 
+        public AprioriRepository(NpgsqlConnection connection)
+        {
+            _connection = connection;
+
+            if (_connection.State != System.Data.ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
+
+        public List<IOrderedEnumerable<string>> GetAll2ItemSets(IList<string> words)
+        {
+            List<IOrderedEnumerable<string>> twoItemSets = new List<IOrderedEnumerable<string>>();
+
+            string sql = string.Empty;
+
+            // Build some sql
+
+            using(NpgsqlCommand command = new NpgsqlCommand(sql, _connection))
+            {
+                using (NpgsqlDataReader rdr = command.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        List<string> twoItemSet = new List<string>();
+
+                        for (int i = 0; i < rdr.FieldCount; i++)
+                        {
+                            twoItemSet.Add((string)rdr[i]);
+                        }
+
+                        twoItemSets.Add(twoItemSet.OrderBy(word => word));
+                    }
+                }
+            }
+
+            return twoItemSets;
         }
 
         public int GetCountDouble(string word1, string word2)
